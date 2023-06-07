d1def548302f1d4757642be28f13335ebb9a7d68
Suda Kabo <koji333@me.com>
2023-06-07 14:29:27 +0900

    back to initial version

--- main.go
+++ main.go
@@ -63,7 +63,6 @@ func helloRunHandler(w http.ResponseWriter, r *http.Request) {
 	if err := tmpl.Execute(w, data); err != nil {
 		msg := http.StatusText(http.StatusInternalServerError)
 		log.Printf("template.Execute: %v", err)
-		log.Printf("template.Execute2: %v", err)
 		http.Error(w, msg, http.StatusInternalServerError)
 	}
 }
